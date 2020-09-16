0bb2ac0637d820866d066267739be9d2faa22cf0
Quentin Rossé <quentin.rosse@num.uk>
2020-09-16 10:12:07 +0100

    support for assuming AWS Role for uploading the file

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM amazon/aws-cli:2.0.38
 ARG IMG_NAME
 ENV IMG_NAME $IMG_NAME
 
+RUN yum -y install jq
+
 WORKDIR /
 
 COPY data /upload
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# assume AWS role if one is provided
+if [ ! -z "$AWS_ROLE" ]; then
+  echo "AWS_ROLE was set, assuming the role"
+  JSON=$(aws sts assume-role --role-arn "$AWS_ROLE" --role-session-name "upload-modules-files-to-S3")
+  exit_code=$?
+  if [ ! $exit_code = 0 ];  then
+    echo "Failed to assume role, exiting with code $exit_code"
+    exit $exit_code
+  fi
+
+  export AWS_ACCESS_KEY_ID=$(echo ${JSON}     | jq --raw-output ".Credentials[\"AccessKeyId\"]")
+  export AWS_SECRET_ACCESS_KEY=$(echo ${JSON} | jq --raw-output ".Credentials[\"SecretAccessKey\"]")
+  export AWS_SESSION_TOKEN=$(echo ${JSON}     | jq --raw-output ".Credentials[\"SessionToken\"]")
+  echo "using access key id: $AWS_ACCESS_KEY_ID"
+fi
+
+
+
 echo "Deleting old files"
 aws s3 rm s3://$MODULE_S3_BUCKET/ --recursive
 exit_code=$?
