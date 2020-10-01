aac72dffb2c2ff683d7eef9c41beca7042bdc0a8
Tony Walmsley <tony@aosd.co.uk>
2020-10-01 15:29:38 +0100

    Basic schema for the Contacts module - needs work.

---
+++ schema-tests.sh
@@ -0,0 +1,25 @@
+#
+# This script depends on `nodejs`, `npm`, and `ajv` which should be installed using `npm install -g ajv-cli`
+#
+
+# Tests for module 1 - the Contacts module
+success=true
+for f in $(find ./tests/1 -name '*.json'); do
+  echo "checking $f..."
+  out=$(ajv validate -s data/1/schema.json -d $f)
+  if ! [ $? = 0 ]; then
+    echo "error on file $f"
+    success=false
+  fi
+done
+
+# Tests for module 2 - etc. TO-DO
+
+#set relevant exit code
+if [ "$success" = true ]; then
+  echo "tests successful"
+  exit 0
+else
+  echo "tests failed. Please check the logs for more details"
+  exit 1
+fi
